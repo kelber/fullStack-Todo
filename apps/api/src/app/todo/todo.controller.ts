37453fe8d5f7e989c1b5db407a9933cc985eb09c
Kelber Stuchi <kelberstuchi@gmail.com>
2019-09-28 18:58:46 -0300

    nest js done

---
+++ apps/api/src/app/todo/todo.controller.ts
@@ -0,0 +1,47 @@
+import { Controller, Get, Body, Post, Put, Delete, Param } from '@nestjs/common';
+import { Todo } from './../../../../../libs/api-interfaces/src/lib/todo';
+import { Observable, of } from 'rxjs';
+import { TodoService } from './todo.service';
+
+
+
+@Controller('todo')
+export class TodoController {
+
+    constructor(
+        private todoService: TodoService
+    ) {}
+
+    @Get() 
+        findAll(): Observable<Todo[]> {
+            return of(this.todoService.findAll());
+        }
+
+    @Get('id')
+    findOne(@Param('id') id): Observable<Todo> {
+        return of(this.todoService.find(id));
+    }
+
+    @Post()
+    async create(@Body() todo: Todo) {
+        this.todoService.create(todo);
+        return todo;
+    }
+
+    @Delete(':id')
+    async remove(@Param('id') id: number): Promise<void> {
+        return this.todoService.remove(id);
+    }
+
+    // Pass @Param and @Body
+    @Put(':id')
+    update(@Param('id') id: number, @Body() updateTodo: Todo):  Observable<Todo> {
+        return of(this.todoService.update(id, updateTodo));
+    }
+   
+
+
+
+
+
+}
---
+++ apps/api/src/app/todo/todo.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@nestjs/common';
+import { Todo } from './../../../../../libs/api-interfaces/src/lib/todo';
+
+
+export const mockTodos: Todo[] = [
+    { id: 0, title: 'Discover NgrX Data', description: 'Discover', active: false },
+    { id: 1, title: 'Test new IronMan armor', description: 'Iron Man', active: true },
+];
+
+@Injectable()
+export class TodoService {
+    public todos: Todo[] = [];
+    
+    constructor() {}
+
+    create(todo: Todo) {
+        this.todos.push(todo);
+    }
+
+    findAll(): Todo[] {
+        return mockTodos;
+    }
+
+    find(id: number): Todo {
+        return this.todos.find(item => {
+            return item.id = id;
+        });
+    }
+
+    remove(id: number): void {
+        this.todos.filter(item => {
+            return item.id != id;
+        })
+    }
+
+    update(id: number, todo: Todo): Todo {
+        const tmpIndex = this.todos.findIndex(obj => obj.id == id);
+        const updatedObject = { ...this.todos[tmpIndex], ...todo };
+        this.todos = [
+            ...this.todos.slice(0, tmpIndex),
+            updatedObject,
+            ...this.todos.slice(tmpIndex + 1)
+        ];
+        return updatedObject;
+        }
+    
+}
