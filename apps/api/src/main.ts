e7ceab3a82356b12c4edf3e9364b9905a791b67d
Kelber Stuchi <kelberstuchi@gmail.com>
2019-09-28 19:23:50 -0300

    added swagger way

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,17 +4,34 @@
  **/
 
 import { NestFactory } from '@nestjs/core';
-
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger way
+  // const options = new DocumentBuilder()
+  //   .setTitle('api')
+  //   .setDescription('Todos API description')
+  //   .setVersion('1.0')
+  //   .addTag('todos')
+  //   .build();
+  // const document = SwaggerModule.createDocument(app, options);
+  // SwaggerModule.setup('api', app, document);
+
+  // await app.listen(3333);
+
+  
+  // Default way
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.port || 3333;
   await app.listen(port, () => {
     console.log('Listening at http://localhost:' + port + '/' + globalPrefix);
   });
+
+  
 }
 
 bootstrap();
